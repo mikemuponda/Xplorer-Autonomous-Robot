51107dbae0f90d19ddedcb8ba199918c3ba1c6b8
Mike <mupondamike@gmail.com>
2018-11-05 01:22:18 -0500

    working on compound map with bumper events

--- src/mapping.cpp
+++ src/mapping.cpp
@@ -1,19 +1,20 @@
 #include <ros/ros.h>
 #include <ca_msgs/Bumper.h>
+#include "mike_mapping/map.h"
 #include <nav_msgs/OccupancyGrid.h>
-#include <mike_mapping/map.h>
+#include <grid_map_ros/grid_map_ros.hpp>
+#include <nav_msgs/Odometry>
+
+using namespace grid_map;
+
+//Need another listener and callback for nav messages odometry to get position
 
 class Listener 
 {
   public:
-    double min;
-    double max;
-    double theta;
-    double point;
-    double x;
-    double y;
-    double X [360];
-    double Y [360];
+    bool left_bumper;
+    bool right_bumper;
+   
 
      void scanCallback(const ca_msgs::Bumper::ConstPtr& msg);
 };
@@ -21,52 +22,52 @@ class Listener
 class Listener2 
 {
   public:
-    double min;
-    double max;
-    double theta;
-    double point;
-    double x;
-    double y;
-    double X [360];
-    double Y [360];
-
+    bool convert;
+    GridMap map({"elevation"});
      void scanCallback2(const nav_msgs::OccupancyGrid::ConstPtr& msg);
 };
 Listener listener;
 Listener2 listener2;
 
+
 void Listener::scanCallback(const ca_msgs::Bumper::ConstPtr& msg) 
 {
-    min=msg->range_min;
-    max=msg->range_max;
-    theta=0.0;
-    point=0.0;
-    x=0.0;
-    y=0.0;
+   left_bumper=msg->is_left_pressed;
+   right_bumper=msg->is_right_pressed;
+
+  //if either condition true, we need to mark that spot on the map and relative to the bumper contact side
     
    
 }
 
 void Listener2::scanCallback2(const nav_msgs::OccupancyGrid::ConstPtr& msg) 
 {
-   
+
+   //convert occupancy grid to grid map
+   convert = grid_map::GridMapRosConverter::fromOccupancyGrid(msg,"elevation",map);
+
+  //Need an iterator to go through grid to find th right cells to mark according to odometry data
+   	
    
 }
     
 
 int main(int argc,char** argv)
 {
-    ros::init(argc,argv,"Compound_Map_node");
+    //Choice..create 2 maps then concatenate or create one map and add new events 
+
+
+    ros::init(argc,argv,"new_map_node");
     ros::NodeHandle nh;
-    ros::Subscriber sub = nh.subscribe("/map",100,&Listener::scanCallback,&listener);
-    ros::Subscriber sub = nh.subscribe("/bumper",100,&Listener2::scanCallback2,&listener2);
+    ros::Subscriber sub = nh.subscribe("/bumper",100,&Listener::scanCallback,&listener);
+    ros::Subscriber sub2 = nh.subscribe("/map",100,&Listener2::scanCallback2,&listener2);
+    
+    //Does Matt want grid_map or Occupancy grid
+    ros::Publisher publisher = nh.advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
 
     ros::Publisher pub =nh.advertise<mike_mapping::map>("/new_map",1000);
-    //ros::Publisher pub = nh.advertise<visualization_msgs::Marker>("Occupancy",100);
-
     ros::Rate loop_rate(100);
    
-    uint32_t shape = visualization_msgs::Marker::CUBE;
 
     int count=0;
    
@@ -77,8 +78,9 @@ int main(int argc,char** argv)
   	msg.header.stamp=ros::Time::now();
   	msg.header.frame_id="/MSG_FRAME_TODO";
 
-  	  if(/*bumper event*/){
-   	    msg.new_map=100;
+  	  if(1==2/* Add bumper event*/){
+   	   // msg.new_map=SOMETHING;
+           //case right bumper, case left bumper & case both bumpers
    	   }
    
     	 pub.publish(msg);
