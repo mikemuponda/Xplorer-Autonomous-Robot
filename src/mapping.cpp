f3bee708a7f8f67e152f578eabb45e9ed6708fe1
Mike <mupondamike@gmail.com>
2018-11-07 16:54:00 -0500

    commit 3: mapping update, code still incomplete, workspace issues resolved

--- src/mapping.cpp
+++ src/mapping.cpp
@@ -3,19 +3,17 @@
 #include "mike_mapping/map.h"
 #include <nav_msgs/OccupancyGrid.h>
 #include <grid_map_ros/grid_map_ros.hpp>
+#include <grid_map_msgs/grid_map_msgs.hpp>  //Need to verify this include
 #include <nav_msgs/Odometry>
 
 using namespace grid_map;
-
-//Need another listener and callback for nav messages odometry to get position
 
 class Listener 
 {
   public:
     bool left_bumper;
     bool right_bumper;
-   
-
+  
      void scanCallback(const ca_msgs::Bumper::ConstPtr& msg);
 };
 
@@ -24,11 +22,23 @@ class Listener2
   public:
     bool convert;
     GridMap map({"elevation"});
+
      void scanCallback2(const nav_msgs::OccupancyGrid::ConstPtr& msg);
 };
+
+class Listener3 
+{
+  public:
+    double x;
+    double y;
+    double z;
+
+     void scanCallback3(const nav_msgs::Odometry::ConstPtr& msg);
+};
+
 Listener listener;
 Listener2 listener2;
-
+Listener3 listener3;
 
 void Listener::scanCallback(const ca_msgs::Bumper::ConstPtr& msg) 
 {
@@ -37,7 +47,6 @@ void Listener::scanCallback(const ca_msgs::Bumper::ConstPtr& msg)
 
   //if either condition true, we need to mark that spot on the map and relative to the bumper contact side
     
-   
 }
 
 void Listener2::scanCallback2(const nav_msgs::OccupancyGrid::ConstPtr& msg) 
@@ -46,8 +55,15 @@ void Listener2::scanCallback2(const nav_msgs::OccupancyGrid::ConstPtr& msg)
    //convert occupancy grid to grid map
    convert = grid_map::GridMapRosConverter::fromOccupancyGrid(msg,"elevation",map);
 
-  //Need an iterator to go through grid to find th right cells to mark according to odometry data
-   	
+}   
+
+void Listener3::scanCallback3(const nav_msgs::Odometry::ConstPtr& msg) 
+{
+
+   //get our position in terms of xyz
+   x=msg->pose.pose.position.x;
+   y=msg->pose.pose.position.y;
+   z=msg->pose.pose.position.z;
    
 }
     
@@ -61,6 +77,7 @@ int main(int argc,char** argv)
     ros::NodeHandle nh;
     ros::Subscriber sub = nh.subscribe("/bumper",100,&Listener::scanCallback,&listener);
     ros::Subscriber sub2 = nh.subscribe("/map",100,&Listener2::scanCallback2,&listener2);
+    ros::Subscriber sub2 = nh.subscribe("/odom",100,&Listener3::scanCallback3,&listener3);
     
     //Does Matt want grid_map or Occupancy grid
     ros::Publisher publisher = nh.advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
@@ -69,7 +86,12 @@ int main(int argc,char** argv)
     ros::Rate loop_rate(100);
    
 
-    int count=0;
+     int count=0;
+
+     //fixed distance between bumper and center of robot, make this measurement..maybe split into x & y offsets
+     double rbtbmprXoffst;
+     double rbtbmprYoffst
+
    
      while(ros::ok()){
 
@@ -78,10 +100,19 @@ int main(int argc,char** argv)
   	msg.header.stamp=ros::Time::now();
   	msg.header.frame_id="/MSG_FRAME_TODO";
 
-  	  if(1==2/* Add bumper event*/){
-   	   // msg.new_map=SOMETHING;
-           //case right bumper, case left bumper & case both bumpers
-   	   }
+        //iterator to go through grid to find the right cells to mark according to odometry data
+   	for (grid_map::GridMapIterator iterator(listener2.map); !iterator.isPastEnd(); ++iterator)
+           {
+               if(listener.left_bumper)
+                  {
+   	          // msg.new_map=SOMETHING;
+   	          }
+                 
+               if(listener.right_bumper){
+                 //Do something to grid map and add to msg
+                }
+           }
+
    
     	 pub.publish(msg);
          ros::spinOnce();
