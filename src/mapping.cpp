61650e8baac00c1be2006d7157f4f58570bd38bd
Mike <mupondamike@gmail.com>
2018-11-16 17:20:44 -0500

    Mapping ovewritting issue psuedo code, need to implement line iterator as well'

--- src/mapping.cpp
+++ src/mapping.cpp
@@ -1,6 +1,5 @@
 #include <ros/ros.h>
 #include <ca_msgs/Bumper.h>
-#include "mike_mapping/map.h"
 #include <nav_msgs/OccupancyGrid.h>
 #include <grid_map_ros/grid_map_ros.hpp>
 #include <grid_map_core/grid_map_core.hpp>
@@ -8,9 +7,9 @@
 #include <nav_msgs/Odometry.h>
 #include <std_msgs/Int8MultiArray.h>
 #include <grid_map_ros/GridMapRosConverter.hpp>
-#include <string>
+#include <string.h>
 #include "nav_msgs/MapMetaData.h"
-
+#include <stdlib.h> 
 using namespace grid_map;
 
 class Bumper 
@@ -26,7 +25,13 @@ class Occupancy
 {
   public:
    bool convert;
+   double cell_size;
+   double width;
+   double height;
+   bool isInitialized;
+   std::string frame_id;
    GridMap map;
+   //make this a pointer if you run into problems
 
      void occupancyCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg);
 };
@@ -47,18 +52,32 @@ Odom odom;
 
 void Bumper::bumperCallback(const ca_msgs::Bumper::ConstPtr& msg) 
 {
+   if(occupancy.isInitialized){
    left_bumper=msg->is_left_pressed;
    right_bumper=msg->is_right_pressed;
+   }
     
 }
 
 void Occupancy::occupancyCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg) 
 {
-   
+   //Get occupancy grid parameters and set new grid map parameters
+   //Either create 2 layers or create 2 maps
+   //you want one object to be permanent and the other to be concatenated as they come in
+if(isInitialized){
    map.setFrameId("map");
-   //map.setGeometry(Length(1.2, 2.0), 0.03);
-   map.add("occupancy");
-   convert=GridMapRosConverter::fromOccupancyGrid(*msg,"occupancy",map);
+   width=msg->info.width;
+   height=msg->info.height;
+   frame_id=msg->header.frame_id;
+   cell_size=msg->info.resolution;
+   map.setGeometry(Length(height, width), cell_size);
+   map.add("occupancy", -1.0);
+   Position origin=Position(msg->info.origin.position.x, msg->info.origin.position.y);
+   map.setPosition(origin);
+   isInitalized=true;
+}
+//try to do as much of the code as possible within the callback
+   GridMapRosConverter::fromOccupancyGrid(*msg,"occupancy", map);
    
 }   
 
@@ -84,43 +103,79 @@ int main(int argc,char** argv)
     ros::Subscriber occu_sub = nh.subscribe("/map",100,&Occupancy::occupancyCallback,&occupancy);
     ros::Subscriber odom_sub= nh.subscribe("/odom",100,&Odom::odomCallback,&odom);
     
-    //Does Matt want grid_map or Occupancy grid
-    ros::Publisher publisher = nh.advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
-    //ros::Publisher publisher = nh.advertise<nav_msgs::OccupancyGrid>("new_occupancy_grid",1);
-    ros::Publisher pub =nh.advertise<mike_mapping::map>("/new_map",1000);
-    ros::Rate loop_rate(100);
+    //Remember to uncomment and broadcast Occupancy grid
+    ros::Publisher publisher = nh.advertise<nav_msgs::OccupancyGrid>("/new_occupancy_grid",1);
+    ros::Rate loop_rate(30);
      
     
 
      int count=0;
 
-     //fixed distance between bumper and center of robot, make this measurement..maybe split into x & y offsets
-     double rbtbmprXoffst;
-     double rbtbmprYoffst;
+     //fixed distance between bumper and center of robot, radius to bumper is about 6.83 inches
+     double rbtbmprXoffst=0.174;
+     double rbtbmprYoffst=0.174;
 
    
      while(ros::ok()){
 
-    	mike_mapping::map msg;
-    
+    	nav_msgs::OccupancyGrid msg;
   	msg.header.stamp=ros::Time::now();
-  	msg.header.frame_id="/MSG_FRAME_TODO";
-
+  	msg.header.frame_id=occupancy.frame_id;
+        msg.info.map_load_time=ros::Time::now();
+        msg.info.resolution=occupancy.cell_size;
+        msg.info.width=occupancy.width;
+        msg.info.height=occupancy.height;
+        
+        //This improves efficiciency in traversing grid_map 
+        grid_map::Matrix& data = occupancy.map["occupancy"];
         //iterator to go through grid to find the right cells to mark according to odometry data
    	for (grid_map::GridMapIterator iterator(occupancy.map); !iterator.isPastEnd(); ++iterator)
            {
+              Position curr_pos;
+              occupancy.map.getPosition(*iterator, curr_pos); 
+
+              Position odom_pos=Position(odom.x,odom.y);
+              Position bmprOffst=Position(0.0,0.0);
+             
+
+              Index curr_index;
+              occupancy.map.getIndex(curr_pos,curr_index);
+
+              Index dest_index;
+              
+             
+              //OVERSIMPLIFIED VERSION, Marking Bumper detected obstacle positions
                if(bumper.left_bumper)
                   {
-   	          // msg.new_map=SOMETHING;
+                    if(curr_pos==odom_pos){
+                          occupancy.map.atPosition("occupancy",odom_pos+bmprOffst)=100;
+                      }
+
+                //local space
+                //global 
+                    //case going forward
+                    //case left
+                    //case right
+                    //case backwards
    	          }
                  
                if(bumper.right_bumper){
-                 //Do something to grid map and add to msg
-                }
-           }
+                   if(curr_pos==odom_pos){
+                        occupancy.map.atPosition("occupancy",odom_pos+bmprOffst)=100; 
+                      }
+                 }
 
-   
-    	 pub.publish(msg);
+              /** fix lidar blind spots
+               if(occupancy.map.atPosition("occupancy", odom_pos)==-1){
+                  Position maxPos=Position(odom.x+0.00,odom.y+0.00);
+                  while(curr_pos < maxPos){
+                      }
+                }
+            **/ 
+           }
+         Publish as grid map
+         GridMapRosConverter::toOccupancyGrid(occupancy.map,"occupancy",0,100,msg);
+    	 publisher.publish(msg);
          ros::spinOnce();
          loop_rate.sleep();
          count++;
